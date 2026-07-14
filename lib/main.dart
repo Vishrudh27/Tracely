93232db011951b0716b30106a8f59ead8936b570
Vishrudh27 <vishrudh27@gmail.com>
2026-07-14 23:30:44 +0530

    chore: setup project architecture and core dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('THIS IS BUILD TEST '),
+            const Text('Vishrudh +'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
